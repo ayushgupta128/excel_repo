32b8ddc37920b112d789331a754711e3c5cd3da5
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2026-09-17 12:31:07 +0000

    Add email gem to send processed data as Excel attachment in `p1` pipeline

--- demo_folder/pipelines/p1.py
+++ demo_folder/pipelines/p1.py
@@ -10,5 +10,19 @@ Schedules = [Schedule(
 args = PipelineArgs(label = "p1", version = 1, auto_layout = False, schedules = Schedules)
 
 with Pipeline(args) as pipeline:
+    email_cust_events = Process(
+        name = "email_cust_events",
+        properties = Email(
+          body = "",
+          subject = "Pipeline Success",
+          includeData = True,
+          fileName = "file1.xlsx",
+          to = ["g.ayush@prophecy.io"],
+          fileFormat = "xlsx",
+          hasTemplate = False
+        ),
+        output_ports = None,
+        comment = "Sends an email with the pipeline success update and attaches the processed data as an Excel file."
+    )
     p1__reformat_1 = Process(name = "p1__Reformat_1", properties = ModelTransform(modelName = "p1__Reformat_1"))
-
+    p1__reformat_1 >> email_cust_events
